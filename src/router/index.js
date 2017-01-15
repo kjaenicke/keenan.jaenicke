71ef301c0b5ce648004424412325366b1cc0c27b
Keenan Jaenicke <kjaenick@gmail.com>
2017-01-15 13:10:12 -0500

    switch to hashrouter for now

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, hashHistory } from 'react-router'
 import App from '../containers/App';
 
 const Routes = () => (
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path='/' component={App} />
   </Router>
 );
