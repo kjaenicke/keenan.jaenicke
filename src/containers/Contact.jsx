c2d86103d6bfed0c412bd33aedc9789c3b199071
Keenan Jaenicke <kjaenick@gmail.com>
2017-01-17 22:10:18 -0500

    adds more info on contact, updates some styles, fix typo

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -8,8 +8,12 @@ const classes = {
     ...common.pageLayout.pageHeader,
     color: colors.white
   },
+  contactWrapper: {
+    height: '100%'
+  },
   contact: {
     display: 'flex',
+    flex: 1,
     flexDirection: 'column',
     height: '100%',
     backgroundColor: colors.lightGreen
@@ -30,24 +34,38 @@ const classes = {
         color: colors.black
       }
     }
+  },
+  followMe: {
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center',
+    flex: 1,
+    color: colors.white,
+    backgroundColor: colors.lightGreen,
+    justifyContent: 'center'
   }
 };
 
 const Contact = ({ sheet: {classes} }) => (
-  <div className={classes.contact}>
-    <h3 className={classes.header}>
-      Contact
-    </h3>
-    <div className={classes.icons}>
-      <a href="https://www.linkedin.com/in/KeenanJae" target="_blank">
-        <i className="fa fa-linkedin-square" aria-hidden="true"></i>
-      </a>
-      <a href="https://twitter.com/KeenanJae" target="_blank">
-        <i className="fa fa-twitter-square" aria-hidden="true"></i>
-      </a>
-      <a href="https://github.com/kjaenicke" target="_blank">
-        <i className="fa fa-github-square" aria-hidden="true"></i>
-      </a>
+  <div className={classes.contactWrapper}>
+    <div className={classes.contact}>
+      <h3 className={classes.header}>
+        Contact
+      </h3>
+      <div className={classes.icons}>
+        <a href="https://www.linkedin.com/in/KeenanJae" target="_blank">
+          <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+        </a>
+        <a href="https://twitter.com/KeenanJae" target="_blank">
+          <i className="fa fa-twitter-square" aria-hidden="true"></i>
+        </a>
+        <a href="https://github.com/kjaenicke" target="_blank">
+          <i className="fa fa-github-square" aria-hidden="true"></i>
+        </a>
+      </div>
+      <div className={classes.followMe}>
+        <p>Let's get in touch! Please follow me on Github, Twitter, and/or Github!</p>
+      </div>
     </div>
   </div>
 );
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -80,7 +80,7 @@ const Home = ({ sheet: {classes} }) => (
       <img className={classes.me} src={me} alt="me in new york city" />
       <p className={classes.bio}>
         Hey there! I'm Keenan Jaenicke. I enjoy running, reading, hiking, playing guitar, and being outdoors.
-        I live in Indianapolis with my girlfriend and two dogs.  I graduate from Purdue University in 2013 with
+        I live in Indianapolis with my girlfriend and two dogs.  I graduated from Purdue University in 2013 with
         a degree in Computer Information Technology and have worked as a software developer since then.
       </p>
     </div>
