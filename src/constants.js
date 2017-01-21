3763e123414b60c96460c34ab1fb72880f91128c
Keenan Jaenicke <kjaenick@gmail.com>
2017-01-21 18:06:49 -0500

    fix api endpoint

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const __DEV__ = process.env.NODE_ENV === 'development' ? 'development' : 'production';
+export const __DEV__ = process.env.NODE_ENV === 'development';
 export const API_BASE = __DEV__ ? 'http://localhost:3001' : 'https://powerful-ocean-39419.herokuapp.com';
